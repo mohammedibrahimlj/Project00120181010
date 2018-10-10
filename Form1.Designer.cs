4b2f867c0af8f153223c85e9bc31a919c2104373
Mohammed Ibrahim <mohammedibrahimlj@users.noreply.github.com>
2018-10-10 15:29:46 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ScoreBox = new System.Windows.Forms.TextBox();
+            this.TotalBox = new System.Windows.Forms.TextBox();
+            this.CountBox = new System.Windows.Forms.TextBox();
+            this.AverageBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score Total:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Scores Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Average:";
+            // 
+            // ScoreBox
+            // 
+            this.ScoreBox.Location = new System.Drawing.Point(123, 46);
+            this.ScoreBox.MaxLength = 2;
+            this.ScoreBox.Name = "ScoreBox";
+            this.ScoreBox.Size = new System.Drawing.Size(100, 20);
+            this.ScoreBox.TabIndex = 4;
+            this.ScoreBox.TextChanged += new System.EventHandler(this.ScoreBox_TextChanged);
+            this.ScoreBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // TotalBox
+            // 
+            this.TotalBox.Enabled = false;
+            this.TotalBox.Location = new System.Drawing.Point(123, 75);
+            this.TotalBox.Name = "TotalBox";
+            this.TotalBox.Size = new System.Drawing.Size(100, 20);
+            this.TotalBox.TabIndex = 5;
+            // 
+            // CountBox
+            // 
+            this.CountBox.Enabled = false;
+            this.CountBox.Location = new System.Drawing.Point(123, 104);
+            this.CountBox.Name = "CountBox";
+            this.CountBox.Size = new System.Drawing.Size(100, 20);
+            this.CountBox.TabIndex = 6;
+            // 
+            // AverageBox
+            // 
+            this.AverageBox.Enabled = false;
+            this.AverageBox.Location = new System.Drawing.Point(123, 130);
+            this.AverageBox.Name = "AverageBox";
+            this.AverageBox.Size = new System.Drawing.Size(100, 20);
+            this.AverageBox.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Display Scores";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear Scores";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(175, 202);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 237);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AverageBox);
+            this.Controls.Add(this.CountBox);
+            this.Controls.Add(this.TotalBox);
+            this.Controls.Add(this.ScoreBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Score Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ScoreBox;
+        private System.Windows.Forms.TextBox TotalBox;
+        private System.Windows.Forms.TextBox CountBox;
+        private System.Windows.Forms.TextBox AverageBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApplication
+{
+    public partial class Form1 : Form
+    {
+        List<int> ScoreList;
+        bool valcheck = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ScoreList = new List<int>();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ScoreList.Count <= 4)
+                {
+                    if (!string.IsNullOrEmpty(ScoreBox.Text.Trim().ToString()))
+                    {
+                        ScoreList.Add(int.Parse(ScoreBox.Text.ToString()));
+                        ScoreCalculation();
+                        ScoreBox.Text = "";
+                    }
+                }
+                else
+                {
+                    ScoreBox.Text = "";
+                    MessageBox.Show("Demo Application cannot able to add more that 4 scores !!!!", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error Message", MessageBoxButtons.OK);
+
+            }
+        }
+        public void ScoreCalculation()
+        {
+            int scoreTotal=0;
+            foreach(int scores in ScoreList)
+            {
+                scoreTotal = scoreTotal + scores;
+            }
+            TotalBox.Text = scoreTotal.ToString();
+            AverageBox.Text = int.Parse((scoreTotal / ScoreList.Count()).ToString()).ToString();
+            CountBox.Text = (ScoreList.Count()).ToString();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+         {
+            if(((e.KeyChar<47) || (e.KeyChar > 58)) && (e.KeyChar!=8))
+            {
+                valcheck = false;
+                MessageBox.Show("Enter only numbers", "Error Message", MessageBoxButtons.OK);
+            }
+        }
+
+        private void ScoreBox_TextChanged(object sender, EventArgs e)
+        {
+            if(!valcheck)
+            {
+                valcheck = true;
+                ScoreBox.Text = ScoreBox.Text.ToString().Substring(0, ScoreBox.Text.ToString().Length-1);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ScoreList = new List<int>();
+            ScoreBox.Text = "";
+            AverageBox.Text = "";
+            ScoreBox.Text = "";
+            CountBox.Text = "";
+            TotalBox.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg=string.Empty;
+            int i = 0;
+            ScoreList.Sort();
+            foreach (int scores in ScoreList)
+            {
+                i++;
+                if (i ==1)
+                {
+                    msg =  scores.ToString();
+                }
+                else
+                {
+                    msg = msg +"\n" +scores.ToString();
+                }
+            }
+            MessageBox.Show(msg, "Sorted scores", MessageBoxButtons.OK);
+        }
+    }
+}
